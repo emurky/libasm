43cc9efbff51468118425625b78a7cffdf2d28ee
Safar Safarly <hafl@me.com>
2021-09-26 17:50:27 +0300

    ft_read and ft_write are written and tested

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
-SRCS				=	ft_strlen.s
-#						ft_strcpy.s \
-#						ft_strcmp.s \
-#						ft_strdup.s \
-#						ft_write.s \
-#						ft_read.s \
+SRCS				=	ft_strlen.s \
+						ft_write.s \
+						ft_read.s \
+						# ft_strcpy.s \
+						# ft_strcmp.s \
+						# ft_strdup.s
+
 
 OBJS				= $(SRCS:.s=.o)
 
--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include	<stdio.h>
+#include	<errno.h>
+#include	<fcntl.h>
+#include	<unistd.h>
+#define BUFFER_SIZE		42
+
+extern int	errno;
 
 unsigned int	ft_strlen(char *str);
+int				ft_write(int fd, void *buf, unsigned int nbyte);
+int				ft_read(int fd, void *buf, unsigned int nbyte);
 
 int	main(void)
 {
-	const char	*str = "salam aleykum";
+	char	*str = "salam aleykum\n";
+	char	buffer[BUFFER_SIZE + 1];
+	int		fd = open("ft_strlen.s", O_RDONLY);
+	
+	buffer[BUFFER_SIZE] = '\0';
 	printf("%d ft_strlen\n", ft_strlen((char *)str));
+	ft_write(1, "\n", 1);
+
+	ft_write(1, str, ft_strlen(str));
+	perror("ft_write test");
+	ft_write(1, "\n", 1);
+
+	errno = 0;
+
+	ft_read(fd, buffer, BUFFER_SIZE);
+	ft_write(1, buffer, BUFFER_SIZE);
+	ft_write(1, "\n", 1);
+	perror("ft_read test");
+	ft_write(1, "\n", 1);
+
 }
